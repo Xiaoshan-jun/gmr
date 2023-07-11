52295ca381b592eb974560b010d3ce54df3f5b50
Xiaoshan-jun <90107845+Xiaoshan-jun@users.noreply.github.com>
2023-07-11 16:35:09 -0700

    Update kdecollisionprob.py

--- kdecollisionprob.py
+++ kdecollisionprob.py
@@ -63,62 +63,65 @@ def myKDE(data, grid_points):
 
 def main():     # instant collision probability
 
-    f1t0 = np.load('F1T0.npy')  # 1000by3
-    f2t0 = np.load('F2T0.npy')  # 1000by3
+    for i in range(10):
+        name1 = 'F1T' + str(i) + '.npy'
+        name2 = 'F2T' + str(i) + '.npy'
+        f1t0 = np.load(name1)  # 1000by3
+        f2t0 = np.load(name2)  # 1000by3
 
-    dr = f1t0 - f2t0
-
-
-    n_ds = f1t0.shape[0]
-
-    sep_dis_min = 305
-
-
-    grid_points = 20
-
-
-
-    # kde
-    datarange, X1, Y1, Z1, den = myKDE(dr, grid_points)  # kde for random vector dr
-
-    xgrid, ygrid, zgrid = np.mgrid[datarange[0]:datarange[1]:grid_points * 1j, datarange[2]:datarange[3]:grid_points * 1j, datarange[4]:datarange[5]:grid_points * 1j]
-    # xgrid = xgrid.flatten()
-    # ygrid = ygrid.flatten()
-    grid_points_x = np.linspace(datarange[0], datarange[1], num=grid_points)
-    grid_points_y = np.linspace(datarange[2], datarange[3], num=grid_points)
-    grid_points_z = np.linspace(datarange[4], datarange[5], num=grid_points)
-
-
-
-
-
-
-
-    # collision prob kde
-    mat_bin = np.zeros((grid_points, grid_points, grid_points))
-    for i in range(0,grid_points):
-        for j in range(0,grid_points):
-            for k in range(0,grid_points):
-                if (grid_points_x[i] ** 2 + grid_points_y[j]** 2 + grid_points_z[k]** 2) < sep_dis_min**2:
-                    mat_bin[i][j] = 1
-
-    mat_select = np.multiply(den, mat_bin)
-    prob_kde = np.sum(mat_select) * 1.0 / np.sum(den)
-    print("prob of instant collision calc by KDE:", prob_kde)    # 0.14
-
-
-
-
-
-
-    # Monte carlo calculate probability of collision      instant probability
-    cnt = 0
-    for i in range(n_ds):
-        if (dr[i,0]**2 + dr[i,1]**2 + dr[i,2]**2) <= sep_dis_min**2:
-            cnt = cnt + 1
-
-    prob_mc = (cnt*1.0)/n_ds
-    print("prob of instant collision calc by MC:", prob_mc)  # 0.13
+        dr = f1t0 - f2t0
+    
+    
+        n_ds = f1t0.shape[0]
+    
+        sep_dis_min = 10
+    
+    
+        grid_points = 20
+    
+    
+    
+        # kde
+        datarange, X1, Y1, Z1, den = myKDE(dr, grid_points)  # kde for random vector dr
+    
+        xgrid, ygrid, zgrid = np.mgrid[datarange[0]:datarange[1]:grid_points * 1j, datarange[2]:datarange[3]:grid_points * 1j, datarange[4]:datarange[5]:grid_points * 1j]
+        # xgrid = xgrid.flatten()
+        # ygrid = ygrid.flatten()
+        grid_points_x = np.linspace(datarange[0], datarange[1], num=grid_points)
+        grid_points_y = np.linspace(datarange[2], datarange[3], num=grid_points)
+        grid_points_z = np.linspace(datarange[4], datarange[5], num=grid_points)
+    
+    
+    
+    
+    
+    
+    
+        # collision prob kde
+        mat_bin = np.zeros((grid_points, grid_points, grid_points))
+        for i in range(0,grid_points):
+            for j in range(0,grid_points):
+                for k in range(0,grid_points):
+                    if (grid_points_x[i] ** 2 + grid_points_y[j]** 2 + grid_points_z[k]** 2) < sep_dis_min**2:
+                        mat_bin[i][j] = 1
+    
+        mat_select = np.multiply(den, mat_bin)
+        prob_kde = np.sum(mat_select) * 1.0 / np.sum(den)
+        print("prob of instant collision calc by KDE:", prob_kde)    # 0.14
+    
+    
+    
+    
+    
+    
+        # Monte carlo calculate probability of collision      instant probability
+        cnt = 0
+        for i in range(n_ds):
+            if (dr[i,0]**2 + dr[i,1]**2 + dr[i,2]**2) <= sep_dis_min**2:
+                cnt = cnt + 1
+    
+        prob_mc = (cnt*1.0)/n_ds
+        print("prob of instant collision calc by MC:", prob_mc)  # 0.13
 
 
 
